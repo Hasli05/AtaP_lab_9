187d8c8e4f769affae026e534f3873856183517b
msxku <msxku@LEGION3060>
2022-05-06 17:59:45 +0300

    Добавьте файлы проекта.

---
+++ AtaP_lab_9.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    double y, x, t, xFirst, xSecond, xThird ;
+    t = 0;
+    x = 0;
+
+    for (t ; t > 5; t += 0.25)
+    {
+        xFirst = t + 8;
+        if (xFirst <= 0.5)
+        {
+            return x = (double)xFirst;
+            t = 0;
+            break;
+        }
+    }
+
+
+    for (; t > 5; t += 0.25)
+    {
+        xSecond = sin(t);
+        if ((xSecond > 0.5) && (xSecond <= 1))
+        {
+            return x = (double)xSecond;
+            t = 0;
+            break;
+        }
+    }
+
+
+    for (t ; t > 5; t += 0.25)
+    {
+        xThird = log10(t) + 2;
+        if (xThird > 1)
+        {
+            return x = (double)xThird;
+            t = 0;
+            break;
+        }
+    }
+
+
+    y = sqrt(abs(x + 3)) + 5.6 * x;
+    cout << "" << y << endl;
+
+}
+
